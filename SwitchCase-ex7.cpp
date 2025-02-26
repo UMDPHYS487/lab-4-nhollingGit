d433f0d7426506f16c31d3afa7750dbbc5ca6146
nhollingGit <nholling@terpmail.umd.edu>
2025-02-26 18:25:54 +0000

    Completed excersises 6 and 7

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,18 @@
-// add your code here
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    int res = 0;
+    cin >> N;
+    res = 2*(N < 10 && N % 2 == 0) + (N % 2 == 1);
+    switch (res)
+    {
+        case 2: cout << N*N << "\n";
+            break;
+        case 1: cout << N/2.0 << "\n";
+            break;
+        default: cout << "hahaha\n";
+    }
+    return 0;
+}
--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,18 @@
-// Add your code here
+#include <iostream>
+using namespace std;
+
+int factorial(int i) {
+    int res = 1;
+    for(i; i>0; i--) {
+        res *= i;
+    }
+    return res;
+}
+
+int main() {
+    int N;
+    cout << "Please enter an interger number: ";
+    cin >> N;
+    cout << "The value you entered is " << N << " and its factorial is " << factorial(N) << endl;
+    return 0;
+}
